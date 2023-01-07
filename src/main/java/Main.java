ec947aed1858f9fa8d9420b75d323a657b60e572
potrap <potrapmax@gmail.com>
2023-01-08 00:22:57 +0200

    added method minus added methos minus

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    public static int sum(int a,int b) {
+        return a + b;
+    }
+    public static int minus(int a, int b){
+        return a-b;
+    }
+}
